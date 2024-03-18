339a4bb16f1f104826c4441389d5acc012dabca2
Viktor Dyukov <snailrake@mail.ru>
2024-03-18 19:19:46 +0400

    Initial commit

---
+++ src/test/java/org/tele2/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tele2.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
